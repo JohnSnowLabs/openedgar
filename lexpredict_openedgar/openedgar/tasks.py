cf5cad8aa16989764102fe53cd2760d0f4e39121
Jose Juan Martinez <36634572+josejuanmartinez@users.noreply.github.com>
2022-05-09 18:52:08 +0200

    Update tasks.py

--- lexpredict_openedgar/openedgar/tasks.py
+++ lexpredict_openedgar/openedgar/tasks.py
@@ -415,7 +415,7 @@ def process_filing(client, file_path: str, filing_buffer: Union[str, bytes] = No
     # Create filing document records
     try:
         #create_filing_documents(client, filing_data["documents"], filing, store_raw=store_raw, store_text=store_text)
-        create_filing_documents(client, filing_data["documents"], filing, store_raw=False, store_text=False)
+        create_filing_documents(client, filing_data["documents"], filing, store_raw=False, store_text=True)
         filing.is_processed = True
         filing.is_error = False
         filing.save()
