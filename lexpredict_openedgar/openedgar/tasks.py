34bbe0b02248433e811a3dfdd011bc5dd6392573
Jose Juan Martinez <36634572+josejuanmartinez@users.noreply.github.com>
2022-05-09 20:31:07 +0200

    Update tasks.py

--- lexpredict_openedgar/openedgar/tasks.py
+++ lexpredict_openedgar/openedgar/tasks.py
@@ -108,7 +108,7 @@ def create_filing_documents(client, documents, filing, store_raw: bool = True, s
             raw_path = pathlib.Path(S3_DOCUMENT_PATH, "text", document["sha1"]).as_posix()
             if not client.path_exists(raw_path):
                 logger.info("-- Starting to process store_text... raw_path={}".format(raw_path))
-                client.put_buffer(raw_path, document["content_text"].encode('utf-8'), write_bytes=False)
+                client.put_buffer(raw_path, document["content_text"].encode('utf-8'), write_bytes=True)
                 logger.info("Uploaded text contents for filing={0}, sequence={1}, sha1={2}"
                             .format(filing, document["sequence"], document["sha1"]))
             else:
